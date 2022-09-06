11bda5aecd616392fc7a83e505a5196c2690997c
initdc <initd@outlook.com>
2022-09-06 09:37:12 +0000

    fix: add missing mount point `/run`

--- mount.rb
+++ mount.rb
@@ -8,12 +8,14 @@ def unmountImage path
 	`umount #{path}`
 end
 
+# ref: https://wiki.alpinelinux.org/wiki/How_to_make_a_cross_architecture_chroot
 def mountDev path
 	puts "mounting /dev on  #{path}"
 	`mount -t proc /proc #{path}/proc`
 	`mount -t sysfs /sys #{path}/sys`
 	`mount -o bind /dev #{path}/dev`
 	`mount -o bind /dev/pts #{path}/dev/pts`
+	`mount -o bind /run #{path}/run`
 end
 
 def unmountDev path
@@ -22,6 +24,7 @@ def unmountDev path
 	`umount #{path}/sys`
 	`umount #{path}/dev/pts`
 	`umount #{path}/dev`
+	`umount #{path}/run`
 end
 
 # img = "ubuntu-core-gnome.img"

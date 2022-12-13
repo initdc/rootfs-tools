dcb95e651e05cde659a669ea8bb0e78e4159bc3c
initdc <initd@outlook.com>
2022-12-14 07:48:13 +0800

    feat: add copy_kernel func, update .gitignore

--- copy.rb
+++ copy.rb
@@ -41,5 +41,76 @@ class Copy
     end
 end
 
+# https://launchpad.net/~canonical-kernel-team/+archive/ubuntu/ppa/+build/24173564
+def copy_kernel src, dest, extra = []
+
+    file_paths = %W[
+        /boot/
+        /lib/firmware/
+        /lib/modprobe.d/
+        /lib/modules/
+        /usr/lib/linux/
+
+        /etc/fstab
+    ]
+    file_paths = file_paths + extra
+
+    exec_dir = Dir.pwd
+
+    `mkdir -p #{dest}`
+    Dir.chdir dest do
+        file_paths.each do |file_path|
+            cp_src = "#{src}#{file_path}"
+            dest_path = file_path.delete_prefix("/")
+
+            if dest_path.end_with?("/")
+                is_file = false
+            else
+                is_file = true
+            end
+
+            parent_arr = dest_path.split("/")
+            parent_arr.pop()
+            # p parent_arr
+
+            case parent_arr.size
+            when 0
+                parent_dir = ""
+            when 1
+                parent_dir = parent_arr[0]
+            else
+                parent_dir = parent_arr.join("/")
+            end
+
+            if parent_dir != ""
+                `mkdir -p #{parent_dir}`
+                Dir.chdir parent_dir do
+                    puts Dir.pwd
+
+                    cmd = "cp -a #{cp_src} ."
+                    puts cmd
+                    system cmd
+                    puts
+                end
+            else
+                puts Dir.pwd
+
+                cmd = "cp -a #{cp_src} ."
+                puts cmd
+                system cmd
+                puts
+            end
+        end
+    end
+end
+
 # Copy.Fedora "cache/Fedora-Container-Base-36-1.5.x86_64.tar.xz", "fd-36"
 # Copy.Arch "cache/archlinux-bootstrap-2022.09.03-x86_64.tar.gz", "arch"
+
+extra = %W[
+    /usr/lib/linux-allwinner-5.17-tools-5.17.0-1003/
+    /usr/lib/libcpupower.so.5.17.0-1003
+    /usr/lib/linux-tools/
+]
+
+copy_kernel "/media/ubuntu/cloudimg-rootfs", "origin"
